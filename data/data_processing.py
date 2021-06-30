96b0ea7deaafa774181e9651c0d44235bec036cf
11ray <raruidol@dsic.upv.es>
2021-06-30 19:24:12 +0200

    first approach for input data structuring (argument classification task)

--- data/data_processing.py
+++ data/data_processing.py
@@ -1,4 +1,34 @@
+import json
+
 # Create 2 different data structures from input data to approach arg. classification and ID retrieval.
 
 
+def argument_classification_data(filepath):
+    with open(filepath) as file:
+        minute_file = json.load(file)
+        file.close()
+
+    output = []
+    for session in minute_file:
+        for intervention in session['proceeding']:
+            # sentence segmentation of utterances
+            utterance_segment_list = intervention['utterance'].split('\n')
+            # if the intervention contains money expressions
+            if len(intervention['moneyExpressions']) > 0:
+                # look for the segments which contain the money expression
+                for expression in intervention['moneyExpressions']:
+                    segments = ''
+                    for utterance_segment in utterance_segment_list:
+                        # check if the money expression is included in the utterance, and that the money expression is not a subset of a higher value mon. expression
+                        if expression['moneyExpression'] in utterance_segment and not utterance_segment[utterance_segment.rfind(expression['moneyExpression'])-1].isdigit():
+                            # build composition of segments where mon. expr. appears
+                            segments = segments + utterance_segment
+
+                    output.append([expression['moneyExpression'], expression['argumentClass'], segments])
+
+    return output
+
+
 # Rebuild the original json files with the outputs of the algorithm.
+
+
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import os
+from data import data_processing as preproc
+
+train_path = 'data/dry-run-data/train'
 
 if __name__ == "__main__":
-    print('hello')
+
+    input_data = []
+
+    for _, _, files in os.walk(train_path):
+        for file in files:
+            input_data.append(preproc.argument_classification_data('data/dry-run-data/train/'+file))
+
+    print(input_data)
